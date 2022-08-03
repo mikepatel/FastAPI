8588cae7adfc320141f971b9377e29a21c74aaca
Michael <michael.patel94@gmail.com>
2022-08-02 20:13:08 -0400

    Adding logging

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ File description:
 # Imports
 from datetime import datetime
 from fastapi import FastAPI
+from loguru import logger
 from pydantic import BaseModel
 from typing import Optional
 
@@ -46,6 +47,38 @@ async def get_player():
 async def create_player(player: Player):
     output = f'Creating a new player called {player.first_name} {player.last_name} who plays for the {player.team}'
     return output
+
+
+@app.get("/log_test")
+async def log():
+    placeholder = "test"
+    logger.debug(placeholder)
+    logger.info(placeholder)
+    logger.warning(placeholder)
+    logger.error(placeholder)
+    logger.critical(placeholder)
+    return placeholder
+
+
+@app.get("/log_exception")
+@logger.catch
+async def log_exception():
+    # testing KeyError
+    teams = {
+        "East": [
+            "Boston Celtics",
+            "Philadelphia 76ers",
+            "New York Knicks",
+            "Milwaukee Bucks"
+        ],
+        "West": [
+            "LA Lakers",
+            "Memphis Grizzlies",
+            "Phoenix Suns",
+            "Dallas Mavericks"
+        ]
+    }
+    return teams["Europe"]
 
 
 ################################################################################
